f2e98af37d3de418f5a356aca9ae550af8e6f28e
Oliver Cordes <ocordes@astro.uni-bonn.de>
2021-03-29 14:51:07 +0200

    - migrate to jupyterlab > 3.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,16 @@
 import {
-  JupyterFrontEnd, JupyterFrontEndPlugin, IRouter
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
-import { Widget } from '@lumino/widgets';
-
-import { ITopBar } from "jupyterlab-topbar";
-
-import '@jupyterlab/application/style/buttons.css';
-
-import '../style/index.css';
-
+/**
+ * Initialization data for the jupyterlab-logout extension.
+ */
 const extension: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-logout',
+  id: 'jupyterlab-logout:plugin',
   autoStart: true,
-  requires: [IRouter, ITopBar],
-  activate: async (
-    app: JupyterFrontEnd,
-    router: IRouter,
-    topBar: ITopBar,
-  ) => {
-    const logout = document.createElement('a');
-    logout.id = "logout";
-    logout.innerHTML = "Log Out";
-    logout.addEventListener('click', function () {
-      router.navigate('/logout', { hard: true });
-    });
-
-    const widget = new Widget({node: logout});
-    widget.addClass('jp-Button-flat');
-    topBar.addItem("logout-button", widget);
+  activate: (app: JupyterFrontEnd) => {
+    console.log('JupyterLab extension jupyterlab-logout is activated!');
   }
 };
 
