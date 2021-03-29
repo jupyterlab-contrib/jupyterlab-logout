645659ae57d0d528c8cc92525c30d284c8cf18df
Oliver Cordes <ocordes@astro.uni-bonn.de>
2021-03-29 15:52:32 +0200

    - fix eslint problems

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import {
-  JupyterFrontEnd, JupyterFrontEndPlugin, IRouter
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin,
+  IRouter
 } from '@jupyterlab/application';
 
 import { Widget } from '@lumino/widgets';
 
-import { ITopBar } from "jupyterlab-topbar";
+import { ITopBar } from 'jupyterlab-topbar';
 
 import '@jupyterlab/application/style/buttons.css';
 
@@ -14,21 +16,17 @@ const extension: JupyterFrontEndPlugin<void> = {
   id: 'jupyterlab-logout',
   autoStart: true,
   requires: [IRouter, ITopBar],
-  activate: async (
-    app: JupyterFrontEnd,
-    router: IRouter,
-    topBar: ITopBar,
-  ) => {
+  activate: async (app: JupyterFrontEnd, router: IRouter, topBar: ITopBar) => {
     const logout = document.createElement('a');
-    logout.id = "logout";
-    logout.innerHTML = "Log Out";
-    logout.addEventListener('click', function () {
+    logout.id = 'logout';
+    logout.innerHTML = 'Log Out';
+    logout.addEventListener('click', () => {
       router.navigate('/logout', { hard: true });
     });
 
-    const widget = new Widget({node: logout});
+    const widget = new Widget({ node: logout });
     widget.addClass('jp-Button-flat');
-    topBar.addItem("logout-button", widget);
+    topBar.addItem('logout-button', widget);
   }
 };
 
